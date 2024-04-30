8e97db615aece4ff3eef10000e315551ca1e386d
Min0814 <nhm0814@naver.com>
2024-04-30 21:27:01 +0900

    결과

--- cl.py
+++ cl.py
@@ -18,15 +18,15 @@ res.raise_for_status()
 
 # soup 객체 만들기
 soup = BeautifulSoup(res.text, "lxml")
-announcementBOX = soup.find('dlv', attrs={"class": "bbs_list01"}) # 전체 영역에서 'a' 태그를 찾지 않고 인기 급상승 영역으로 범위 제한
+announcementBOX = soup.find('div', attrs={"class": "bbs_list01"}) # 전체 영역에서 'a' 태그를 찾지 않고 인기 급상승 영역으로 범위 제한
 announcement = announcementBOX.find_all('a') # 인기 급상승 영역에서 'a'태그 모두 찾아 변수 announcement에 할당
 
 i = 1
 
 # 반복문으로 제목 가져오기(터미널 창 출력 및 엑셀 저장)
-for announcement in announcement: 
-  href = announcement.get("") 
-  print(f"{str(i)}위: {href}")
-  data = [str(i), href]
-  writer.writerow(data)
-  i += 1
+for announcement1 in announcement: 
+    title = announcement1.get_text(strip=True)  # 텍스트 추출
+    print(f"{str(i)}위: {title}")
+    data = [str(i), title]
+    writer.writerow(data)
+    i += 1
